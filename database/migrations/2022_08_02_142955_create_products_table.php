c3e79f68188a088c918b37b76e9a3db03c344776
JoshuaNathan1 <135298681+JoshuaNathan1@users.noreply.github.com>
2025-01-16 21:58:00 +0700

    database, lang, public

---
+++ database/migrations/2022_08_02_142955_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_supplier');
+            $table->string('product_name', 25)->unique();
+            $table->text('orientation')->nullable();
+            $table->text('description');
+            $table->integer('price');
+            $table->integer('stock');
+            $table->integer('discount')->nullable();
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
